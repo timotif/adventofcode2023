7994620f57eb563cf773b1ab12872a8804363416
Timoti Fregni <timotif@gmail.com>
2023-12-10 21:02:47 +0100

    day 9

---
+++ 09/01_main.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   01_main.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 16:27:04 by tfregni           #+#    #+#             */
+/*   Updated: 2023/12/09 16:46:01 by tfregni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void printVec(std::vector<int> &v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int allZeros(std::vector<int> &v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        if (v[i] != 0) {
+            return (0);
+        }
+    }
+    return (1);
+}
+
+int compute(std::vector<int> &v) {
+    std::vector<int> next;
+    bool end = true;
+    for (size_t i = 0; i < v.size() - 1; i++) {
+        int diff = v[i + 1] - v[i];
+        next.push_back(diff);
+        if (diff != 0)
+            end = false;
+    }
+    if (end)
+        return (v.front());
+    v.insert(v.begin(), v.front() - compute(next));
+    return (v.front());
+}
+
+int main(void) {
+    std::ifstream file;
+    file.open("input.txt");
+    std::string line;
+    int sum = 0;
+    while (std::getline(file, line)) {
+        std::vector<int> v;
+        while (line.find(" ") != std::string::npos) {
+            v.push_back(std::stoi(line));
+            line = line.substr(line.find(" ") + 1);
+        }
+        v.push_back(std::stoi(line));
+        sum += compute(v);  
+    }
+    std::cout << "Sum: " << sum << std::endl;
+}
