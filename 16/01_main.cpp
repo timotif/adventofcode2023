a4ebf1053adacec71c3b751eb932deaae4d4cc94
tim <timotif@gmail.com>
2023-12-18 22:50:17 +0100

    part 2

--- 16/01_main.cpp
+++ 16/01_main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   00_main.cpp                                        :+:      :+:    :+:   */
+/*   01_main.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tfregni <tfregni@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 22:49:59 by tfregni           #+#    #+#             */
-/*   Updated: 2023/12/18 22:50:00 by tfregni          ###   ########.fr       */
+/*   Created: 2023/12/18 22:49:57 by tfregni           #+#    #+#             */
+/*   Updated: 2023/12/18 22:49:58 by tfregni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,10 @@ void printGrid(std::vector<std::string> grid) {
         std::cout << grid[i] << std::endl;
 }
 
-int main(void) {
-    std::fstream file("input.txt");
-    if (!file.is_open()) {
-        std::cout << "Error opening file" << std::endl;
-        return 1;
-    }
-    std::vector<std::string> grid;
-    std::string line;
-    while (std::getline(file, line))
-        grid.push_back(line);
+int computeEnergized(std::tuple<int, int, int, int> pos, std::vector<std::string> grid) {
     std::set<std::tuple<int, int, int, int>> seen;
     std::deque<std::tuple<int, int, int, int>> path;
-    std::tuple<int, int, int, int> cur = {0, -1, 0, 1};
+    std::tuple<int, int, int, int> cur = pos;
     path.push_back(cur);
     while (path.size() > 0) {
         cur = path.back();
@@ -121,5 +112,28 @@ int main(void) {
         std::tuple<int, int> tmp = {std::get<ROW>(*it), std::get<COL>(*it)};
         energized.insert(tmp);
     }
-    std::cout << "Energized: " << energized.size() << std::endl;
+    return energized.size();
+}
+
+int main(void) {
+    std::fstream file("input.txt");
+    if (!file.is_open()) {
+        std::cout << "Error opening file" << std::endl;
+        return 1;
+    }
+    std::vector<std::string> grid;
+    std::string line;
+    while (std::getline(file, line))
+        grid.push_back(line);
+    
+    int energized = 0;
+    for (size_t i = 0; i < grid.size(); i++) {
+        energized = std::max(energized, computeEnergized(std::make_tuple(i, -1, 0, 1), grid));
+        energized = std::max(energized, computeEnergized(std::make_tuple(i, grid[0].size(), 0, -1), grid));
+    }
+    for (size_t i = 0; i < grid[0].size(); i++) {
+        energized = std::max(energized, computeEnergized(std::make_tuple(-1, i, 1, 0), grid));
+        energized = std::max(energized, computeEnergized(std::make_tuple(grid.size(), i, -1, 0), grid));
+    }
+    std::cout << "Energized: " << energized << std::endl;
 }
